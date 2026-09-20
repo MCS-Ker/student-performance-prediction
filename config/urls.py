53402754089679001ea8fdfd5845cdeae0cf24d6
Rasha Alkhayer <ralkhayer69@gmail.com>
2026-09-20 18:17:03 +0300

    Add root URL redirect to dashboard

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from students import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='dashboard'), name='home'),
 
     # صفحات المصادقة
     path('login/', auth_views.LoginView.as_view(
