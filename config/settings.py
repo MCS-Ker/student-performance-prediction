55395804503be37058b165304b76a318b01fecf9
Rasha Alkhayer <ralkhayer69@gmail.com>
2026-09-20 17:49:24 +0300

    Configure production settings for deployment

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wn#9wk4&!$w8el43(nu*^_dak^(flxxan0x$ni465q05bkqk5w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mcsker.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
